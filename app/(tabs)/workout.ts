7c99becaee23aac42e289b327e554a182f3dad9e
benie20-G <iratuzibeneigiramata@gmail.com>
2025-05-09 11:05:00 +0200

    improved readability

---
+++ app/(tabs)/workout.ts
@@ -0,0 +1,23 @@
+export const workouts = [
+  {
+    id: 1,
+    title: 'Upper Body Strength',
+    duration: '45 min',
+    difficulty: 'Intermediate',
+    image: 'https://images.pexels.com/photos/4164761/pexels-photo-4164761.jpeg?auto=compress&cs=tinysrgb&w=800',
+  },
+  {
+    id: 2,
+    title: 'HIIT Cardio Blast',
+    duration: '30 min',
+    difficulty: 'Advanced',
+    image: 'https://images.pexels.com/photos/4498151/pexels-photo-4498151.jpeg?auto=compress&cs=tinysrgb&w=800',
+  },
+  {
+    id: 3,
+    title: 'Core Strengthening',
+    duration: '25 min',
+    difficulty: 'Beginner',
+    image: 'https://images.pexels.com/photos/6551133/pexels-photo-6551133.jpeg?auto=compress&cs=tinysrgb&w=800',
+  },
+];
